2132a9c813b990bd221a3a250420e08a3371c3d6
sml3s <sml3s2g@gmail.com>
2019-08-21 23:06:24 -0300

    H2 database, test profile, JPA

---
+++ src/main/java/com/projetobase/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.projetobase.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetobase.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
