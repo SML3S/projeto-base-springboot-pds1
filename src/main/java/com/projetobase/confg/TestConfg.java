4f300dddab82e901dced035846741cf84e59553b
sml3s <sml3s2g@gmail.com>
2019-09-03 20:13:15 -0300

    Payment, OneToOne association

--- src/main/java/com/projetobase/confg/TestConfg.java
+++ src/main/java/com/projetobase/confg/TestConfg.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.projetobase.entities.Category;
 import com.projetobase.entities.Order;
 import com.projetobase.entities.OrderItem;
+import com.projetobase.entities.Payment;
 import com.projetobase.entities.Product;
 import com.projetobase.entities.User;
 import com.projetobase.entities.enums.OrderStatus;
@@ -82,6 +83,11 @@ public class TestConfg implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
