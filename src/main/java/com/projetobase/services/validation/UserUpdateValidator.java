f4670a71b8ca9cc390067d4671bc6baaad9bd7e9
sml3s <sml3s2g@gmail.com>
2019-09-30 21:59:42 -0300

    Custom user update validation: unique email

---
+++ src/main/java/com/projetobase/services/validation/UserUpdateValidator.java
@@ -0,0 +1,54 @@
+package com.projetobase.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.projetobase.dto.UserDTO;
+import com.projetobase.entities.User;
+import com.projetobase.repositories.UserRepository;
+import com.projetobase.resource.exceptions.FieldMessage;
+
+public class UserUpdateValidator implements ConstraintValidator<UserUpdateValid, UserDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private UserRepository userReportsitory;
+	
+		
+	@Override
+	public void initialize(UserUpdateValid ann) {
+	}
+
+	@Override
+	public boolean isValid(UserDTO dto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>)request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Long uriId = Long.parseLong(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		User user = userReportsitory.findByEmail(dto.getEmail());
+		
+		if(user != null && !user.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "email alredy taken"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
