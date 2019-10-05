f4559b62c35884cafcde56166ee6a3ba4ad45f7a
sml3s <sml3s2g@gmail.com>
2019-10-05 10:38:15 -0300

    Authentication

---
+++ src/main/java/com/projetobase/resource/AuthResource.java
@@ -0,0 +1,26 @@
+package com.projetobase.resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projetobase.dto.CredentialsDTO;
+import com.projetobase.dto.TokenDTO;
+import com.projetobase.services.AuthService;
+
+@RestController
+@RequestMapping(value ="/auth")
+public class AuthResource {
+
+	@Autowired
+	private AuthService service;
+	
+	@PostMapping("/login")
+	public ResponseEntity<TokenDTO> login(@RequestBody CredentialsDTO dto){
+		TokenDTO tokenDTO = service.authenticate(dto);
+		return ResponseEntity.ok().body(tokenDTO);
+	}
+}
--- src/main/java/com/projetobase/resource/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/projetobase/resource/exceptions/ResourceExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.projetobase.services.exceptions.DatabaseException;
+import com.projetobase.services.exceptions.JWTAuthenticationException;
 import com.projetobase.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -45,6 +46,15 @@ public class ResourceExceptionHandler {
 		for(FieldError x : e.getBindingResult().getFieldErrors()) {
 			err.addError(x.getField(), x.getDefaultMessage());
 		}
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(JWTAuthenticationException.class)
+	public ResponseEntity<StandardError> jWTAuthentication(JWTAuthenticationException e, HttpServletRequest request){
+		String error = "Autentication error";
+		HttpStatus status = HttpStatus.UNAUTHORIZED;
+		StandardError err = new StandardError(Instant.now(), status.value(), error,e.getMessage(), request.getRequestURI());
+		
 		return ResponseEntity.status(status).body(err);
 	}
 }
