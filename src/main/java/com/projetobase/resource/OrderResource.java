0d7938cd8aa36e83e56567f36041f440d911ed99
sml3s <sml3s2g@gmail.com>
2019-10-08 21:53:37 -0300

    Order GET endpoints PART 1

--- src/main/java/com/projetobase/resource/OrderResource.java
+++ src/main/java/com/projetobase/resource/OrderResource.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.projetobase.dto.OrderDTO;
+import com.projetobase.dto.OrderItemDTO;
 import com.projetobase.services.OrderService;
 
 @RestController
@@ -30,10 +31,14 @@ public class OrderResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<OrderDTO> findById(@PathVariable Long id){
-	
 		OrderDTO dto = service.findById(id);
-		
 		return ResponseEntity.ok().body(dto) ;
+	}
+	
+	@GetMapping(value = "/{id}/items")
+	public ResponseEntity<List<OrderItemDTO>> findItems(@PathVariable Long id){
+		List<OrderItemDTO> list = service.findItems(id);
+		return ResponseEntity.ok().body(list) ;
 	}
 	
 	@GetMapping(value = "/myorders")
--- src/main/java/com/projetobase/services/OrderService.java
+++ src/main/java/com/projetobase/services/OrderService.java
@@ -2,13 +2,17 @@ package com.projetobase.services;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.projetobase.dto.OrderDTO;
+import com.projetobase.dto.OrderItemDTO;
 import com.projetobase.entities.Order;
+import com.projetobase.entities.OrderItem;
 import com.projetobase.entities.User;
 import com.projetobase.repositories.OrderRepository;
 import com.projetobase.services.exceptions.ResourceNotFoundException;
@@ -39,5 +43,13 @@ public class OrderService {
 		User client = authService.authenticated();
 		List<Order> list = repository.findByClient(client);
 		return list.stream().map(e -> new OrderDTO(e)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public List<OrderItemDTO> findItems(Long id) {
+		Order order = repository.getOne(id);
+		authService.validateOwnOrderOrAdmin(order);
+		Set<OrderItem> set = order.getItem();
+		return set.stream().map(e -> new OrderItemDTO(e)).collect(Collectors.toList());
 	}
 }
